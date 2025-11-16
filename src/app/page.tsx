394acef2f90db1bf69eda2afa34e9114e3d79e41
michael <mike.michaelmilstead@gmail.com>
2025-11-16 14:28:21 -0800

    add github link

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import GithubLink from "@/components/github-link";
 import { InteractableGuitarTabs } from "@/components/guitar-tabs";
 import LatestTamboMessage from "@/components/latest-tambo-message";
 import LatestUserMessage from "@/components/latest-user-message";
@@ -15,6 +16,7 @@ import { TamboProvider } from "@tambo-ai/react";
 export default function Home() {
   return (
     <div className="h-screen flex flex-col justify-center items-center overflow-hidden relative bg-[#232834]">
+      <GithubLink />
       <TamboProvider
         apiKey={process.env.NEXT_PUBLIC_TAMBO_API_KEY!}
         components={components}
@@ -24,8 +26,8 @@ export default function Home() {
         <div className="h-full max-h-1/2 w-full max-w-4xl mx-auto flex flex justify-center items-center">
           {/* Left column: Toolcall and Guitar Tabs */}
           <div className="w-1/2 h-full flex flex-col items-center justify-center p-4">
-            <TamboToolcall />
             <InteractableGuitarTabs />
+            <TamboToolcall />
           </div>
 
           {/* Right column: Messages and Input */}
@@ -35,12 +37,10 @@ export default function Home() {
               <LatestTamboMessage />
             </div>
             <div className="w-full max-w-2xl">
-              <div className="p-4">
-                <MessageInput contextKey="tambo-template">
-                  <MessageInputTextarea placeholder="Ask how to play anything" />
-                  <MessageInputSubmitButton />
-                </MessageInput>
-              </div>
+              <MessageInput contextKey="tambo-template">
+                <MessageInputTextarea placeholder="Ask how to play anything" />
+                <MessageInputSubmitButton />
+              </MessageInput>
             </div>
           </div>
         </div>
